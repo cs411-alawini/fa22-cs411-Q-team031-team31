762160ff84f69313290e73526da0e6e22b9deefa
Jackie Chan <jackiec3@illinois.edu>
2022-11-10 14:41:22 -0600

    Had to implement CORS middleware because Axios requires it. Something about applications on localhost talking to each other giving weird errors.

--- backend/server/api/api.py
+++ backend/server/api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from psql.models import User
 from psql.route import search_routes, average_round_trip_length
@@ -10,6 +11,14 @@ def get_app():
 
 
 app = get_app()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
